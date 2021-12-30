0a5b44ff519ffc07abaa10d5dfd30b708a77e3eb
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-12-30 10:33:36 -0800

    Create pumping_fuel.py

---
+++ Compete/Class of Code/pumping_fuel.py
@@ -0,0 +1,5 @@
+# Problem: https://docs.google.com/document/d/1Nhf-xwfGRw_36FnxcWym0v3l-k-w8idqAlViMPjFCl0/edit?usp=sharing
+
+t, c, r = [int(i) for i in input().split()]
+
+print('yes' if r * t >= c else 'no')

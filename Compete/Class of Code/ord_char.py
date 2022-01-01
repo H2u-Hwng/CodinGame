c9092d125539263c6c76dec21ad4e32f125adf54
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2022-01-01 10:05:29 -0800

    Create ord_char.py

---
+++ Compete/Class of Code/ord_char.py
@@ -0,0 +1,10 @@
+# Problem: https://docs.google.com/document/d/1xzassYoxUu4x1267ZD6QuRb4yigWhDsnWF-6_ZqLPks/edit?usp=sharing
+
+s = input()
+
+sum = 0
+
+for c in s:
+    sum += ord(c)
+    
+print(sum)

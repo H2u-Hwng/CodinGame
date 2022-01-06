25e99af136c6f220165b3294d95c708f735d7356
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2022-01-06 09:59:40 -0800

    Create 0_rectangle.py

---
+++ Compete/Class of Code/0_rectangle.py
@@ -0,0 +1,8 @@
+# Problem: https://docs.google.com/document/d/1B-bTbxNllKj0wbou5h4iaLyzgW3EbF3un0-5QKLVcy0/edit?usp=sharing
+
+h=int(input())
+w=int(input())
+for _ in range(h):
+    for _ in range(w):
+        print('O',end='')
+    print()

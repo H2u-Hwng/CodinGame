54ecd7a80bf9bd9904941ddc6772555246f9ac18
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2022-01-07 23:38:11 -0800

    Create arithmetic_progression.py

---
+++ Compete/Class of Code/arithmetic_progression.py
@@ -0,0 +1,7 @@
+# Problem: https://docs.google.com/document/d/10yn0cJf0FNIXwmImfVU8ZmUm8KWepTY5tiYXStf4isk/edit?usp=sharing
+
+a1 = int(input())
+d = int(input())
+n = int(input())
+
+print(a1 + (n - 1) * d)

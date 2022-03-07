be8696ec33c65c54e28dc02116df0ef8457c9f90
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2022-03-06 21:12:26 -0800

    Create reverse1.py

---
+++ Compete/Class of Code/Reverse Mode/reverse1.py
@@ -0,0 +1,10 @@
+drink = input()
+straw = int(input())
+
+if straw == 1:
+    print('____|____')
+else:
+    print('_________')
+print('\\       /')
+print(' \\  ' + drink + '  /')
+print('  \\___/')
